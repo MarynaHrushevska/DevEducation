712de1250f6535fc087617ee64636b23798d9464
Maryna Hrushevska <mia.grushevska@gmail.com>
2021-04-25 15:36:27 +0300

    without tests and brackets

--- calc/calculator.js
+++ calc/calculator.js
@@ -3,12 +3,10 @@ var Calculator = function() {
     this.operations = new MathOperations();
 }
 Calculator.prototype.initDOMElement = function() {
-    // general 
     this.container = document.getElementById('container');
     this.screen = document.getElementById('screen');
     this.panel = document.getElementById('panel');
     this.engineer = document.getElementById('engineer');
-    this.clear = document.getElementById('clear');
     this.switch = document.getElementById('switch');
 
     this.divide = document.getElementById('divide');
@@ -18,17 +16,6 @@ Calculator.prototype.initDOMElement = function() {
     this.equal = document.getElementById('equal');
     this.dot = document.getElementById('dot');
     this.addMinus = document.getElementById('add-minus');
-
-    this.nine = document.getElementById('nine');
-    this.eight = document.getElementById('eight');
-    this.seven = document.getElementById('seven');
-    this.six = document.getElementById('six');
-    this.five = document.getElementById('five');
-    this.four = document.getElementById('four');
-    this.three = document.getElementById('three');
-    this.two = document.getElementById('two');
-    this.one = document.getElementById('one');
-    this.zero = document.getElementById('zero');
 
     this.leftBracket = document.getElementById('left-bracket');
     this.rightBracket = document.getElementById('right-bracket');
@@ -55,56 +42,20 @@ Calculator.prototype.init = function() {
     this.panel.addEventListener('click', this.handleClick.bind(this));
     this.engineer.addEventListener('click', this.handleClick.bind(this));
     this.switch.addEventListener('click', this.switchCalcs.bind(this));
-    window.addEventListener('keynown', function(e) {
-        if (e.keyCode === 46) {
-            this.screen.textContent.pop();
-        } 
-    });
 }
 Calculator.prototype.switchCalcs = function() {
     this.container.classList.toggle('engineer');
 }
 Calculator.prototype.handleClick = function(event) {
     var button = event.target;
-    var isDigit = button.classList.contains('panel__digit');
-    var isSymbol = button.classList.contains('panel__symbol');
-    var isClear = button.classList.contains('panel__clear');
-    var isDot = button.classList.contains('panel__dot');
-    var isEqual = button.classList.contains('panel__equal');
-    var isEngineer = button.classList.contains('panel__engineer');
+    var isDigit = button.dataset.type === 'digit';
+    var isClear = button.dataset.type === 'clear';
+    var isDot = button.dataset.type === 'dot';
+    var isEqual = button.dataset.type === 'equal';
+    var isUnary = button.dataset.type === 'unary';
+    var isBinary = button.dataset.type === 'binary';
+    var displayResult = button.dataset.type === 'displayResult';
 
-    if (isEngineer) {
-        if (button === this.pi) {
-            this.handlePi();
-        }
-        if (button === this.exponential) {
-            this.handleE();
-        }
-        if (button === this.factorial) {
-            this.handleFactorial();
-        }
-        if (button === this.squareNumber) {
-            this.handleSquare();
-        }
-        if (button === this.tenX) {
-            this.handleTenInX();
-        }
-        if (button === this.cube) {
-            this.handleCube();
-        }
-        if (button === this.mathAbs) {
-            this.handleMathAbs();
-        }
-        if (button === this.oneDivideX) {
-            this.handleOneDivideX();
-        }
-        if (button === this.log) {
-            this.handleLog();
-        }
-        if (button === this.ln) {
-            this.handleLn();
-        }
-    }
     if (isDot) {
         this.handleDot();
     }
@@ -114,30 +65,96 @@ Calculator.prototype.handleClick = function(event) {
         }
         this.screen.textContent += button.textContent;
     }
-    if (isSymbol) {
-        if (button === this.addMinus) {
-            this.handleMinus();
-        } else {
-            this.handleSymbol(button);
-        }
-    } 
+    if (isBinary) {
+        this.handleBinary(button);
+    }
+    if (isUnary) {
+        this.handleUnary(button);
+    }
+    if (displayResult) {
+        this.handleDisplayResult(button);
+    }
     if (isClear) {
         this.screen.textContent = '';
         this.choosenSymbol = '';
         this.firstArg = 0;
         this.secondArg = 0;
-    } 
+    }
     if (isEqual) {
         this.secondArg = this.screen.textContent;
         this.screen.textContent = '';
         this.calcResult();
-        this.choosenSymbol = '';
+        if (this.choosenSymbol) {
+            this.choosenSymbol.classList.remove('activeOperator');
+        }
+        this.choosenSymbol = null;
     }
 }
-Calculator.prototype.handleSymbol = function (button) {
-    this.firstArg = this.screen.textContent;
+Calculator.prototype.handleBinary = function (button) {
+    if (this.choosenSymbol) {
+        this.choosenSymbol.classList.remove('activeOperator');
+    } else {
+        this.firstArg = this.screen.textContent;
+    }
+    this.choosenSymbol = button;
+    this.choosenSymbol.classList.add('activeOperator');
     this.screen.textContent = '';
-    this.choosenSymbol = button.textContent;
+}
+Calculator.prototype.handleDisplayResult = function (button) {
+    switch (button) {
+        case this.pi:
+            this.handlePi();
+            break;
+
+        case this.exponential:
+            this.handleE();
+            break;
+
+        default:
+            break;
+    }
+}
+Calculator.prototype.handleUnary = function (button) {
+    switch (button) {
+        case this.factorial:
+            this.handleFactorial();
+            break;
+
+        case this.squareNumber:
+            this.handleSquare();
+            break;
+
+        case this.tenX:
+            this.handleTenInX();
+            break;
+
+        case this.cube:
+            this.handleCube();
+            break;
+
+        case this.mathAbs:
+            this.handleMathAbs();
+            break;
+
+        case this.oneDivideX:
+            this.handleOneDivideX();
+            break;
+
+        case this.log:
+            this.handleLog();
+            break;
+
+        case this.ln:
+            this.handleLn();
+            break;
+
+        case this.addMinus:
+            this.handleMinus();
+            break;
+    
+        default:
+            break;
+    }
 }
 Calculator.prototype.handleDot = function () {
     if (this.screen.textContent.includes('.')) {
@@ -147,23 +164,29 @@ Calculator.prototype.handleDot = function () {
     this.screen.textContent = this.screen.textContent;
 }
 Calculator.prototype.calcResult = function () {
-    if (this.choosenSymbol === '+') {
+    if (this.choosenSymbol === this.plus) {
         this.screen.textContent = this.operations.sum(this.firstArg, this.secondArg);
     } 
-    if (this.choosenSymbol === '-') {
+    if (this.choosenSymbol === this.subtract) {
         this.screen.textContent = this.operations.subtract(this.firstArg, this.secondArg);
     } 
-    if (this.choosenSymbol === 'x') {
+    if (this.choosenSymbol === this.multiply) {
         this.screen.textContent = this.operations.multiply(this.firstArg, this.secondArg);
     } 
-    if (this.choosenSymbol === '/') {
+    if (this.choosenSymbol === this.divide) {
         this.screen.textContent = this.operations.divide(this.firstArg, this.secondArg);
     }   
-    if (this.choosenSymbol === 'mod') {
+    if (this.choosenSymbol === this.mod) {
         this.screen.textContent = this.operations.mod(this.firstArg, this.secondArg);
+    } 
+    if (this.choosenSymbol === this.powerNumber) {
+        this.screen.textContent = this.operations.powerOfNumber(this.firstArg, this.secondArg);
     } 
 }
 Calculator.prototype.handleMinus = function () {
+    if (this.screen.textContent === '') {
+        return;
+    }
     this.firstArg = this.screen.textContent;  
     this.screen.textContent = this.operations.plusMinus(this.firstArg);
 }
@@ -180,13 +203,6 @@ Calculator.prototype.handleFactorial = function () {
     this.firstArg = this.screen.textContent;
     this.screen.textContent = this.operations.factorial(this.firstArg);
 }
-// Calculator.prototype.deleteNumber = function (pressEvent) {
-//     console.log(2)
-//     if (pressEvent.keyCode === 46) {
-//         console.log(1)
-//         this.screen.textContent = this.screen.textContent.pop();
-//     }  
-// }
 Calculator.prototype.handleTenInX = function () {
     if (this.screen.textContent === '') {
         return;
@@ -243,11 +259,9 @@ Calculator.prototype.handleLn = function () {
         this.screen.textContent = this.operations.naturalLogarithm(this.firstArg);  
     }  
 }
-// Calculator.prototype.handleMod = function () {
-//     if (this.screen.textContent === '' || this.screen.textContent === 0) {
-//         return;
-//     } else {
-//         this.firstArg = this.screen.textContent;
-//         this.screen.textContent = this.operations.mod(this.firstArg, this.secondArg);  
-//     }  
-// }
+
+Calculator.prototype.deleteNumber = function (pressEvent) {
+    if (pressEvent.keyCode === 8) {
+        this.screen.textContent = this.screen.textContent.slice(0, -1);
+    }  
+}
--- calc/script.js
+++ calc/script.js
@@ -2,9 +2,8 @@
 window.addEventListener('DOMContentLoaded', function () {
     var calculator = new Calculator();
     calculator.init();
+
+    window.addEventListener('keyup', function (e) {
+        calculator.deleteNumber(e);
+    })
 });
-// window.addEventListener('keyup', function (e) {
-//     console.log(3);
-//     var deleteLast = new Calculator();
-//     deleteLast.deleteNumber(e);
-// })
