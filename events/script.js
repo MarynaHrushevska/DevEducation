10938bc86961003fea7b5265ae66790a9c9d52e0
Maryna Hrushevska <mia.grushevska@gmail.com>
2021-05-06 00:43:55 +0300

    gallery with UA

--- events/script.js
+++ events/script.js
@@ -8,9 +8,11 @@ function init () {
     const photos = document.getElementById('photos');
     const mainPhoto = document.getElementById('mainPhoto');
     const gallery = document.querySelectorAll('.gallery__slider-photo');
-    const showAll = document.getElementById('showAll')
-    const showAllPopup = document.getElementById('showAllPopup')
-    const closeShowAllPopup = document.getElementById('closeShowAllPopup')
+    const showAll = document.getElementById('showAll');
+    const showAllPopup = document.getElementById('showAllPopup');
+    const closePopup = document.getElementById('closePopup');
+    const showAllPhoto = document.getElementById('showAllPhoto');
+    let timerId;
 
     for (let i of gallery) {
         i.addEventListener('mouseover', () => {
@@ -20,21 +22,42 @@ function init () {
             i.classList.remove('smooth');
         })
     }
+    mainPhoto.src = gallery[0].src;
 
     showAll.addEventListener('click', () => {
-        showAllPopup.classList.remove('hideShowAllPopup');
+        showPopup(showAllPopup);
+        showPhoto(gallery, showAllPhoto);
+    })
+    closePopup.addEventListener('click', () => {
+        hidePopup(showAllPopup);
+        mainPhoto.src = showAllPhoto.src;
+        clearTimeout(timerId);
     })
     photos.addEventListener('click', displayPhoto);
-    closeShowAllPopup.addEventListener('click', () => {
-        showAllPopup.classList.add('hideShowAllPopup');
-    })
+
+    function showPopup (element) {
+        element.classList.remove('hideShowAllPopup');
+    }
+    function hidePopup (element) {
+        element.classList.add('hideShowAllPopup');
+    }
     function displayPhoto (event) {
         const image = event.target;
         const isPhoto = image.dataset.type === 'photo';
         if (!isPhoto) return;
-
-        mainPhoto.src = event.target.src;
+        mainPhoto.src = image.src;
+    }
+    function showPhoto (gallery, showAllPhoto) {
+        let i = 0;
+        showAllPhoto.src = gallery[i].src;
+        timerId = setInterval(() => {
+            i++;
+            showAllPhoto.src = gallery[i].src;
+            if (i === gallery.length - 1) {
+                i = -1;
+            }
+        }, 2000);        
     }
 }
 
-module.exports = { init };
+// module.exports = { init };
--- events/script.test.js
+++ events/script.test.js
@@ -2,6 +2,23 @@ const { init } = require('./script');
 
 describe('init', function() {
     it('init should be defined', function() {
-        expect(init).toBeDefined()
+        expect(init).toBeDefined();
+    })
+    it('should show popup', function () {
+        const button = document.createElement('button');
+        button.classList.add('hideShowAllPopup');
+        function showPopup (button) {
+            button.classList.remove('hideShowAllPopup')
+        }
+        showPopup(button);
+        expect(button.classList.contains('hideShowAllPopup')).toBe(false);
+    })
+    it('should hide popup', function () {
+        const button = document.createElement('button');
+        function showPopup (button) {
+            button.classList.add('hideShowAllPopup')
+        }
+        showPopup(button);
+        expect(button.classList.contains('hideShowAllPopup')).toBe(true);
     })
 })
