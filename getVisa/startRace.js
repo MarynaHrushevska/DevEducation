dc4c840e3426555e6a91dfc99ca1fe0c5215deb3
Maryna Hrushevska <mia.grushevska@gmail.com>
2021-05-21 00:04:00 +0300

    totally wrong logic

--- getVisa/startRace.js
+++ getVisa/startRace.js
@@ -13,7 +13,22 @@ export default function startRace(participants) {
     const balance3 = document.querySelector('.balance3');
     const balance4 = document.querySelector('.balance4');
     const balance5 = document.querySelector('.balance5');
-    const rndInt = randomIntFromInterval(5, 10);
+    const age1 = document.querySelector('.age1');
+    const age2 = document.querySelector('.age2');
+    const age3 = document.querySelector('.age3');
+    const age4 = document.querySelector('.age4');
+    const age5 = document.querySelector('.age5');
+    const documents1 = document.querySelector('.docs1');
+    const documents2 = document.querySelector('.docs2');
+    const documents3 = document.querySelector('.docs3');
+    const documents4 = document.querySelector('.docs4');
+    const documents5 = document.querySelector('.docs5');
+    const english1 = document.querySelector('.english1');
+    const english2 = document.querySelector('.english2');
+    const english3 = document.querySelector('.english3');
+    const english4 = document.querySelector('.english4');
+    const english5 = document.querySelector('.english5');
+    // const rndInt = randomIntFromInterval(5, 10);
 
     race.addEventListener('click', () => {
         raceTable.classList.remove('hide');
@@ -23,33 +38,80 @@ export default function startRace(participants) {
         assignNames(third, participants[2].name);
         assignNames(fourth, participants[3].name);
         assignNames(fifth, participants[4].name);
-        accCheck(participants[0].balance, rndInt, balance1);
-        accCheck(participants[1].balance, rndInt, balance2);
-        accCheck(participants[2].balance, rndInt, balance3);
-        accCheck(participants[3].balance, rndInt, balance4);
-        accCheck(participants[4].balance, rndInt, balance5);
+
+        accCheck(checkAcc, participants[0].balance, randomIntFromInterval(5, 10), balance1);
+        accCheck(checkAcc, participants[1].balance, randomIntFromInterval(5, 10), balance2);
+        accCheck(checkAcc, participants[2].balance, randomIntFromInterval(5, 10), balance3);
+        accCheck(checkAcc, participants[3].balance, randomIntFromInterval(5, 10), balance4);
+        accCheck(checkAcc, participants[4].balance, randomIntFromInterval(5, 10), balance5);
+        
+        accCheck(checkAge, participants[0].age, randomIntFromInterval(1, 3), age1);
+        accCheck(checkAge, participants[1].age, randomIntFromInterval(1, 3), age2);
+        accCheck(checkAge, participants[2].age, randomIntFromInterval(1, 3), age3);
+        accCheck(checkAge, participants[3].age, randomIntFromInterval(1, 3), age4);
+        accCheck(checkAge, participants[4].age, randomIntFromInterval(1, 3), age5);
+
+        accCheck(checkDocs, participants[0].documents, randomIntFromInterval(10, 20), documents1);
+        accCheck(checkDocs, participants[1].documents, randomIntFromInterval(10, 20), documents2);
+        accCheck(checkDocs, participants[2].documents, randomIntFromInterval(10, 20), documents3);
+        accCheck(checkDocs, participants[3].documents, randomIntFromInterval(10, 20), documents4);
+        accCheck(checkDocs, participants[4].documents, randomIntFromInterval(10, 20), documents5);
+
+        accCheck(checkLang, participants[0].english, randomIntFromInterval(5, 10), english1);
+        accCheck(checkLang, participants[1].english, randomIntFromInterval(5, 10), english2);
+        accCheck(checkLang, participants[2].english, randomIntFromInterval(5, 10), english3);
+        accCheck(checkLang, participants[3].english, randomIntFromInterval(5, 10), english4);
+        accCheck(checkLang, participants[4].english, randomIntFromInterval(5, 10), english5);
     })    
-    function accCheck(data, time, element) {
+    function accCheck(fnc, data, time, element) {
         const accCheck = new Promise((resolve, reject) => {
             setTimeout(() => {
-                if (data >= 2000) {
-                    resolve('You win');
-                } else {
-                    reject(new Error('You lose'));
-                }
+            if (fnc(data)) {
+                resolve('You win');
+              } else {
+                reject(new Error('You lose'));
+              }
             }, time)
         })  
         accCheck.then(() => {
-            element.style.border = '1px solid #ff0';
+            element.style.border = '2px solid #008000';
         }).catch(() => {
-            element.style.border = '1px solid #f00';
+            element.style.border = '2px solid #f00';
         });
-    } 
+    }
+    function checkAcc(data) {
+        if (data >= 2000) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+    function checkAge(data) {
+        if (data < 18 || data > 60) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+    function checkDocs(data) {
+        if (data === '3') {
+            return true; 
+        } else {
+            return false;
+        }
+    }
+    function checkLang(data) {
+        if (data === 'B1' || data === 'B2' || data === 'C1') {
+            return true; 
+        } else {
+            return false;
+        }
+    }
     function assignNames(input, data) {
         input.textContent = data;
     }
     function randomIntFromInterval(min, max) {
-        return Math.floor(Math.random() * (max - min + 1) + min * 1000)
+        return (Math.floor(Math.random() * (max - min + 1) + min)) * 1000;
     }
       
 }
