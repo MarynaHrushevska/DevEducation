a7f881d89e87c67032b3cb65a44d45b9134ac2c8
Maryna Hrushevska <mia.grushevska@gmail.com>
2021-06-22 18:25:29 +0300

    creating database and table

--- express/settings/mysql.js
+++ express/settings/mysql.js
@@ -15,4 +15,18 @@ connection.connect(function (err) {
         console.log("Подключение к серверу MySQL успешно установлено");
     }
 });
+connection.query(`CREATE DATABASE IF NOT EXISTS express`, function(err, results) {
+    if (err) {
+        console.log(err);
+    } else {
+        console.log("Database created");
+    }
+});
+connection.query('CREATE TABLE IF NOT EXISTS books (id INT PRIMARY KEY AUTO_INCREMENT, author varchar(255), title varchar(255), date datetime, image varchar(255),  description varchar(255))', function(err, results) {
+    if (err) {
+        console.log(err);
+    } else {
+        console.log("Table books created");
+    }
+});
 module.exports = connection;
