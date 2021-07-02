400564f0f9ecb92c296da6c511c2ea44b2a38b1c
Maryna Hrushevska <mia.grushevska@gmail.com>
2021-07-02 16:55:02 +0300

    saga

---
+++ react_saga_eclipse/client/src/components/App/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { NotificationContainer } from 'react-notifications';
+
+import Header from '../Header';
+import Movies from '../Movies';
+
+import './App.scss';
+import 'react-notifications/lib/notifications.css';
+
+const App = () => (
+    <div className='app'>
+        <Header />
+        <Movies />
+        <NotificationContainer />
+    </div>
+);
+
+export default App;
